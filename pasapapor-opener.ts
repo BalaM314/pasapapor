f1a383b0f37300d25f2ed77b780c73305ca8c0ed
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-07-06 21:11:01 +0530

    MF26????

--- pasapapor-opener.ts
+++ pasapapor-opener.ts
@@ -24,6 +24,7 @@ const otherDocuments: {
 	mf19: "https://www.cambridgeinternational.org/Images/417318-list-of-formulae-and-statistical-tables.pdf",
 	mf10: "https://papers.gceguide.com/A%20Levels/Mathematics%20-%20Further%20(9231)/Other%20Resources/MF10.pdf",
 	mf20: "https://www.cambridgeinternational.org/images/344616-list-of-formulae-and-statistical-tables.pdf",
+	mf26: "https://www.seab.gov.sg/content/syllabus/alevel/2017Syllabus/ListMF26.pdf",
 });
 
 
